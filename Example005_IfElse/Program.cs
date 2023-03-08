4d0dbaf0916547edfc87078b8c91856d67890efb
Vitaliy <vitalik.shalay2002@yandex.ru>
2023-03-08 11:12:33 +0300

    add Example 005 If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "кристина")
+{
+    Console.WriteLine("Ура, это же Кристина!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
